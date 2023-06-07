7803f2cc4d5423d5497c53bbcb6df5997a9c1030
james26572 <102800087+james26572@users.noreply.github.com>
2023-06-07 15:12:40 +0100

    updates

--- application.py
+++ application.py
@@ -6,22 +6,31 @@ Stripe Sample.
 Python 3.6 or newer required.
 """
 import os
-from flask import Flask, redirect, request,render_template,jsonify
+from flask import Flask, redirect, request,render_template,jsonify,session
 from dotenv import load_dotenv
+import time
 
 load_dotenv()
 
 import stripe
 
 
+
+
+sessions = {}
+session_quantities = {}
+
 products = {"veg":'price_1NAMzjArDOvhvxI9l7GVv3N7',"daru":'price_1NCXIAArDOvhvxI9Lko5dXy8',
             "orchard":"price_1NCXTuArDOvhvxI9zwJQOkRt","gleann oir":"price_1NCXUjArDOvhvxI93b7wUpBc",
             "brie":"price_1NFkbKArDOvhvxI94MxYTFdC","cooleeney":"price_1NFkcOArDOvhvxI9eUCqGA9A"}
-line_items = []
+
+
+
 # This is your test secret API key.
 stripe.api_key = os.getenv("STRIPE_SECRET_KEY")
 
 application = Flask(__name__,static_folder="assets")
+application.secret_key = "hello"
 
 YOUR_DOMAIN = 'http://localhost:4242'
 @application.route('/')
@@ -29,27 +38,62 @@ def home():
     return render_template("index.html")
 @application.route('/shop')
 def shop():
-    return render_template("shop.html")
+    
+    
+    session['user_id'] = str(int(time.time() * 1000))
+    sessions[session.get("user_id")] = []
+
+    
+    quantities = {"veg":0,"daru":0,"orchard":0,"gleann oir":0,
+                  "brie":0,"cooleeney":0}
+    
+    session_quantities[session.get("user_id")] = quantities
+    print(session.get("user_id"))
+
+    return render_template("shop.html",quantities = quantities)
 
 @application.route('/addToCart',methods = ["POST"])
 def addToCart():
     product_id = request.form.get("product_id")
     quantity = request.form.get("quantity")
     product = products[product_id]
+
+    
+
+
+    session_id = session.get("user_id")
+    print(session_id)
+
+    
+
+    
+    
     found = False
-    for item in line_items:
+    for item in sessions[session_id]:
         if item['price'] == product:
-            item['quantity']+=quantity
+            item['quantity']=quantity
+            session_quantities[session.get("user_id")]["product_id"] = quantity
             found = True
     if not found:
         newProduct = {'price':product,
                       'quantity':quantity}
-        line_items.append(newProduct)
+        
+        sessions[session_id].append(newProduct)
+        session_quantities[session.get("user_id")][product_id] = quantity
 
-    return render_template("shop.html")
+    print(session_quantities[session.get("user_id")])
+
+    
+
+    return render_template("shop.html",quantities = session_quantities[session.get("user_id")])
 
 @application.route('/create-checkout-session', methods=['POST','GET'])
 def create_checkout_session():
+
+    
+    
+    session_id = session.get("user_id")
+    line_items = sessions[session_id]
     try:
         checkout_session = stripe.checkout.Session.create(
             line_items=line_items,
@@ -61,6 +105,9 @@ def create_checkout_session():
             success_url=YOUR_DOMAIN + '/success.html',
             cancel_url=YOUR_DOMAIN + '/cancel.html',
             payment_method_types = ["card"],
+            
+            
+            
         )
     except Exception as e:
         return str(e)
